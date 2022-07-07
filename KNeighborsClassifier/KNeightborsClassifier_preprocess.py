14f7d222141a6fba29d1c574662d3b1e134fb2de
Tony <dongheehan4@gmail.com>
2022-07-07 17:28:50 +0900

    explain

--- KNeighborsClassifier/KNeightborsClassifier_preprocess.py
+++ KNeighborsClassifier/KNeightborsClassifier_preprocess.py
@@ -1,3 +1,4 @@
+#k-최근접이웃 (k-Nearest Neighbors)을 이용한 분류 + 데이터 전처리
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -13,16 +14,15 @@ fish_weight = [242.0, 290.0, 340.0, 363.0, 430.0, 450.0, 500.0, 390.0, 450.0, 50
                 7.5, 7.0, 9.7, 9.8, 8.7, 10.0, 9.9, 9.8, 12.2, 13.4, 12.2, 19.7, 19.9]
 
 
-fish_data = np.column_stack((fish_length, fish_weight))
+fish_data = np.column_stack((fish_length, fish_weight)) #2개의 열(특성)
 # print(fish_data, len(fish_data), type(fish_data))
-fish_target = np.concatenate((np.ones(35), np.zeros(14)))
+fish_target = np.concatenate((np.ones(35), np.zeros(14)))   
 # print(fish_target)
 train_input, test_input, train_target, test_target = train_test_split(fish_data, fish_target, stratify=fish_target, random_state=42)
 # print(test_input, test_target)
 kn.fit(train_input, train_target)
 # print(kn.score(test_input, test_target))
 # print(kn.predict([[25,150]]))
-
 
 import matplotlib.pyplot as plt
 # plt.scatter(train_input[:,0], train_input[:,1])
@@ -33,18 +33,19 @@ import matplotlib.pyplot as plt
 # plt.ylabel('weight')
 # plt.show()
 
+#두 특성의 스케일이 다르므로 데이터 전처리가 필요함
+#표준점수(standard score)를 이용한 데이터 처리
 mean = np.mean(train_input, axis=0)
 std = np.std(train_input, axis=0)
 train_scaled = (train_input - mean) / std
-new_d = ([25, 150] - mean) / std
-
+new_d = ([25, 150] - mean) / std    #실험용 데이터, 정답은 1번[도미]
 
 test_scaled = (test_input - mean)/std
 kn.fit(train_scaled, train_target)
 print(kn.score(test_scaled,test_target))
 print(kn.predict([new_d]))
 
-distances, indexes = kn.kneighbors([new_d])
+distances, indexes = kn.kneighbors([new_d]) #가장 가까운 데이터 5개 표시
 # print(train_input[indexes])
 
 plt.scatter(train_scaled[:,0], train_scaled[:,1])
