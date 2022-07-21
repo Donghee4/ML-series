cb74209a95d3581f5c0bfa0baf26cda336def978
Tony <dongheehan4@gmail.com>
2022-07-21 22:35:28 +0900

    mr

--- Regression/multiple regression.py
+++ Regression/multiple regression.py
@@ -1,6 +1,6 @@
-#다중 회귀(multiple regression)123
+#다중 회귀(multiple regression)
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import PolynomialFeatures
 import pandas as pd
 import numpy as np
 
@@ -16,4 +16,5 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
         1000.0])
 train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
 
-
+poly = PolynomialFeatures()
+# 다중 인풋에 의한 회귀
