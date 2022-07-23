9cd0c08d4a161ed91892e9378ab70aae100a8b6e
Tony <dongheehan4@gmail.com>
2022-07-23 23:06:19 +0900

    poly

--- Regression/multiple regression.py
+++ Regression/multiple regression.py
@@ -17,4 +17,3 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
 train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
 
 poly = PolynomialFeatures()
-# 다중 인풋에 의한 회귀 #
