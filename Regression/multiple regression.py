6e21e4f9bd3f5fe6ce4120cd5e305315bee8d649
Tony <dongheehan4@gmail.com>
2022-07-25 16:22:16 +0900

    poly fit and test

--- Regression/multiple regression.py
+++ Regression/multiple regression.py
@@ -1,10 +1,13 @@
 #다중 회귀(multiple regression)
+#인풋은 길이, 높이, 두께, 타켓은 무게
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+
 import pandas as pd
 import numpy as np
-import numpy as np
-from sklearn import calibration
+
+
 
 df = pd.read_csv('https://bit.ly/perch_csv_data')
 perch_full = df.to_numpy()
@@ -18,4 +21,17 @@ perch_weight = np.array([5.9, 32.0, 40.0, 51.5, 70.0, 100.0, 78.0, 80.0, 85.0, 8
         1000.0])
 train_input, test_input, train_target, test_target = train_test_split(perch_full, perch_weight, random_state=42)
 
-poly = PolynomialFeatures()
+#이부분 다시보기-------------------------------------------------
+#다중 인풋 처리
+
+poly = PolynomialFeatures(include_bias = False) #절편항무시
+poly.fit(train_input)
+train_poly = poly.transform(train_input)
+# print(train_poly.shape)
+# print(poly.get_feature_names())
+test_poly = poly.transform(test_input)  #테스트 폴리(테스트 인풋용)
+#---------------------------------------------------------------
+
+lr = LinearRegression()
+lr.fit(train_poly, train_target)
+print(lr.score(test_poly, test_target))
