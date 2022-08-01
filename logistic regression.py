14f7e1e64321414118a6a71d83799766b3d60ab0
Tony <dongheehan4@gmail.com>
2022-08-01 14:17:29 +0900

    lo - r

--- logistic regression.py
+++ logistic regression.py
@@ -1,4 +1,7 @@
-#logistic regression
+#logistic regression 이진 분류 문제에서 클래스 확률을 예측
 import pandas as pd
 fish = pd.read_csv('https://bit.ly/fish_csv_data')
-fish.head()
+print(fish.head())
+# print(pd.unique(fish['Species']))
+
+fish_input = fish[['Weight']]
